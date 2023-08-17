6bf1b4be966e3903a029483e2bfb6a302a76b8c8
Ridho Hakiki <ridhohakiki44@gmail.com>
2023-08-17 15:40:44 +0700

    adding error notif on edit.blade.php if nik already exists

--- resources/views/member/edit.blade.php
+++ resources/views/member/edit.blade.php
@@ -9,7 +9,11 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                        
+
+                @if($errors->has('nik'))
+                    <p class="text-red-500">{{ $errors->first('nik') }}</p>
+                @endif
+
                 <form method="post" action="{{ route('member.update', $member) }}">
                     @csrf
                     @method('patch')
